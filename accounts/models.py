75bbd98af5b41182f4b231952e6ce922b9e39ca5
kariukijoni <kariukijoni@gmail.com>
2023-11-03 19:44:40 +0300

    character_fied search

--- accounts/models.py
+++ accounts/models.py
@@ -45,6 +45,7 @@ class Order(models.Model):
     product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True)
     status=models.CharField(max_length=50,choices=STATUS)
+    note=models.CharField(max_length=1000,null=True)
 
     def __str__(self) -> str:
         return f"{self.customer} => {self.product}"
